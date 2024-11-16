b688acec573a8cd3782d211e3bf1542c6083b955
RomikIzBRU <167847351+RomikIzBRU@users.noreply.github.com>
2024-11-16 12:04:29 +0300

    Лабораторная работа №4

---
+++ Лабораторная работа №4/task_2.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+indent = 4
+ensure_ascii = True
+
+def task() -> None:
+    with open(INPUT_FILENAME, encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=",", lineterminator="\n")
+        lines = [line for line in reader]
+
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as file:
+        json.dump(lines, file, indent=indent, ensure_ascii=ensure_ascii)
+
+
+
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
