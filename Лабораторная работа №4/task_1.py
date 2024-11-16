da60495fa31a9f4e4ca075e213c898b19d3ba6e4
RomikIzBRU <167847351+RomikIzBRU@users.noreply.github.com>
2024-11-16 12:07:29 +0300

    Лабораторная работа

---
+++ Лабораторная работа №4/task_1.py
@@ -0,0 +1,11 @@
+import json
+
+def task() -> float:
+    filename='input.json'
+    with open (filename,'r') as file:
+        data=json.load(file)
+
+    total_sum = sum(item["score"] * item["weight"] for item in data)
+    return round(total_sum, 3)
+
+print(task())
